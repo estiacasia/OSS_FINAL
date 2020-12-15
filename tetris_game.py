fc68d161dc92ecc617fe01502e3b8136153378cb
estiacasia <yoongi0422@gmail.com>
2020-12-15 19:09:45 +0900

    add comment

--- tetris_game.py
+++ tetris_game.py
@@ -11,9 +11,9 @@ from tetris_ai import TETRIS_AI
 
 # TETRIS_AI = None
 
-class Tetris(QMainWindow):
-    def __init__(self):
-        super().__init__()
+class Tetris(QMainWindow):  # QMainWindow 상속
+    def __init__(self): # 파이썬의 생성자명은 __init__ 고정 , 첫번째 고정값은 self 로 self : Tetris
+        super().__init__()  # super()로 기반 클래스의 __init__ 메서드 호출
         self.isStarted = False
         self.isPaused = False
         self.nextMove = None
@@ -21,12 +21,12 @@ class Tetris(QMainWindow):
 
         self.initUI()
 
-    def initUI(self):
-        self.gridSize = 22
-        self.speed = 10
+    def initUI(self): #초기 UI 설정, Qt 내장함수들과 사용자 정의 함수들 사용
+        self.gridSize = 22 # 창 크기
+        self.speed = 10 # 블럭 내려가는 속도 (낮을수록 빠름)
 
-        self.timer = QBasicTimer()
-        self.setFocusPolicy(Qt.StrongFocus)
+        self.timer = QBasicTimer() # 타이머 Qt에서 받아옴
+        self.setFocusPolicy(Qt.StrongFocus) # 위젯이 키보드입력/클릭 수락하게끔
 
         hLayout = QHBoxLayout()
         self.tboard = Board(self, self.gridSize)
@@ -41,7 +41,7 @@ class Tetris(QMainWindow):
         self.start()
 
         self.center()
-        self.setWindowTitle('Tetris')
+        self.setWindowTitle('Tetris') #프로그램 상단 바에 Tetris 표기
         self.show()
 
         self.setFixedSize(self.tboard.width() + self.sidePanel.width(),
@@ -52,20 +52,20 @@ class Tetris(QMainWindow):
         size = self.geometry()
         self.move((screen.width() - size.width()) // 2, (screen.height() - size.height()) // 2)
 
-    def start(self):
+    def start(self): #시작 함수
         if self.isPaused:
             return
 
         self.isStarted = True
         self.tboard.score = 0
-        BOARD_DATA.clear()
+        BOARD_DATA.clear() 
 
         self.tboard.msg2Statusbar.emit(str(self.tboard.score))
 
         BOARD_DATA.createNewPiece()
         self.timer.start(self.speed, self)
 
-    def pause(self):
+    def pause(self): #일시 정지 함수
         if not self.isStarted:
             return
 
@@ -117,19 +117,19 @@ class Tetris(QMainWindow):
 
         key = event.key()
         
-        if key == Qt.Key_P:
+        if key == Qt.Key_P: # p누르면 일시정지
             self.pause()
             return
             
-        if self.isPaused:
+        if self.isPaused: 
             return
-        elif key == Qt.Key_Left:
+        elif key == Qt.Key_Left:    # 왼쪽키 > 좌측 이동
             BOARD_DATA.moveLeft()
-        elif key == Qt.Key_Right:
-            BOARD_DATA.moveRight()
-        elif key == Qt.Key_Up:
+        elif key == Qt.Key_Right:   # 우측 키 > 우측 이동
+            BOARD_DATA.moveRight()  
+        elif key == Qt.Key_Up:      # 위 키 > 블럭 회전
             BOARD_DATA.rotateLeft()
-        elif key == Qt.Key_Space:
+        elif key == Qt.Key_Space:   # 스페이크 키 > 블럭 하강
             self.tboard.score += BOARD_DATA.dropDown()
         else:
             super(Tetris, self).keyPressEvent(event)
@@ -137,7 +137,7 @@ class Tetris(QMainWindow):
         self.updateWindow()
 
 
-def drawSquare(painter, x, y, val, s):
+def drawSquare(painter, x, y, val, s): #블럭 색깔 
     colorTable = [0x000000, 0xCC6666, 0x66CC66, 0x6666CC,
                   0xCCCC66, 0xCC66CC, 0x66CCCC, 0xDAAA00]
 
