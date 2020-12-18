d0455632b75521645f8983f52b8c6f5ed39e15fa
brightalot <70755285+brightalot@users.noreply.github.com>
2020-12-18 17:37:10 +0900

    Update tetris_model.py

--- tetris_model.py
+++ tetris_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+# 게임의 데이터 모델 구현
 import random
 
 class Shape(object):
@@ -66,9 +67,9 @@ class Shape(object):
         return (minX, maxX, minY, maxY)
 
 
-class BoardData(object):
-    width = 10
-    height = 22
+class BoardData(object): #보드 정보
+    width = 10 # 가로
+    height = 22 # 높이
 
     def __init__(self):
         self.backBoard = [0] * BoardData.width * BoardData.height
@@ -77,9 +78,10 @@ class BoardData(object):
         self.currentY = -1
         self.currentDirection = 0
         self.currentShape = Shape()
-        self.nextShape = Shape(random.randint(1, 7))
-
-        self.shapeStat = [0] * 8
+        self.nextShape = Shape(random.randint(1, 7)) #다음블록모양
+        #random.randint(1, 7) 1부터 6까지 랜덤 1개.
+        
+        self.shapeStat = [0] * 8 
 
     def getData(self):
         return self.backBoard[:]
@@ -91,35 +93,43 @@ class BoardData(object):
         return self.currentShape.getCoords(self.currentDirection, self.currentX, self.currentY)
 
     def createNewPiece(self):
+        #새로운모양만들기
         minX, maxX, minY, maxY = self.nextShape.getBoundingOffsets(0)
         result = False
+        #기본 출력값은 False
+        
+   #tryMoveCurrent 의 출력값은 boolean값임.
         if self.tryMoveCurrent(0, 5, -minY):
+        #참인경우. 즉,  tryMoveCurrent -> tryMove함수 출력시 결과값 True
             self.currentX = 5
             self.currentY = -minY
             self.currentDirection = 0
             self.currentShape = self.nextShape
             self.nextShape = Shape(random.randint(1, 7))
-            result = True
+            result = True       #움직인다.
         else:
+            #나머지 경우 즉, False
+            
             self.currentShape = Shape()
             self.currentX = -1
             self.currentY = -1
             self.currentDirection = 0
-            result = False
+            result = False      #못움직임.
         self.shapeStat[self.currentShape.shape] += 1
         return result
 
     def tryMoveCurrent(self, direction, x, y):
-        return self.tryMove(self.currentShape, direction, x, y)
+        return self.tryMove(self.currentShape, direction, x, y) # True or False return.
 
-    def tryMove(self, shape, direction, x, y):
+    def tryMove(self, shape, direction, x, y): # 움직이기 가능한가? 판단.
         for x, y in shape.getCoords(direction, x, y):
-            if x >= BoardData.width or x < 0 or y >= BoardData.height or y < 0:
-                return False
-            if self.backBoard[x + y * BoardData.width] > 0:
-                return False
-        return True
+            if x >= BoardData.width or x < 0 or y >= BoardData.height or y < 0: #보드의 가로나 세로를 벗어날 때
+                return False #불가능합니다.
+            if self.backBoard[x + y * BoardData.width] > 0: #설정값보다 보드의 넓이가 클 때.
+                return False #불가능합니다.
+        return True #가능!
 
+ #블록 움직임 제어
     def moveDown(self):
         lines = 0
         if self.tryMoveCurrent(self.currentDirection, self.currentX, self.currentY + 1):
@@ -156,9 +166,9 @@ class BoardData(object):
             self.currentDirection -= 1
             self.currentDirection %= 4
 
-    def removeFullLines(self):
-        newBackBoard = [0] * BoardData.width * BoardData.height
-        newY = BoardData.height - 1
+    def removeFullLines(self): # 꽉찬 줄 블록 제거하는 함수
+        newBackBoard = [0] * BoardData.width * BoardData.height #새로운 백보드
+        newY = BoardData.height - 1  #새로운 높이
         lines = 0
         for y in range(BoardData.height - 1, -1, -1):
             blockCount = sum([1 if self.backBoard[x + y * BoardData.width] > 0 else 0 for x in range(BoardData.width)])
